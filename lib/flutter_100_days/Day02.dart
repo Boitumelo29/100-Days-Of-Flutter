3f84b255106607c937e5283ff1a69e254577d2a9
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-23 19:37:11 +0200

    update

--- lib/flutter_100_days/Day02.dart
+++ lib/flutter_100_days/Day02.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
 
-//Day 2 is on creating a drawer, 
-//Drawer widget is used tp provide access to different destionations and 
+//Day 2 is on creating a drawer,
+//Drawer widget is used tp provide access to different destionations in the application.
+
+// its properties include, child, hashCode, key, runtimeType, elvation, semanticLabel
+//Drawer({Key key, double elevation: 16.0, Widget child, String semanticLabel})
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final appTitle = "Flutter Day 2";
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
