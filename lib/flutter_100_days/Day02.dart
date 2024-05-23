3eee2a95c1e87ac829e4dbdff0c093d0930d94aa
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-23 19:53:32 +0200

    update

--- lib/flutter_100_days/Day02.dart
+++ lib/flutter_100_days/Day02.dart
@@ -36,6 +36,9 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        body: Center(
+      child: Column(children: []),
+    ));
   }
 }
