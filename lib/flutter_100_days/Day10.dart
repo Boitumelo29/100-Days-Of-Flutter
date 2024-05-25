a80e6f207e6eec8fe565f5b222faaec2eb305152
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-25 16:07:01 +0200

    update

---
+++ lib/flutter_100_days/Day10.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  final String title = "Lazy Loader";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(title: title),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
