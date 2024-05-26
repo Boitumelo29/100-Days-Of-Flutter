33a9e02d2b66c1d98d6481066461bf12f6d1e4c9
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-26 17:21:08 +0200

    update

--- lib/flutter_100_days/Day12_hingeAnimation.dart
+++ lib/flutter_100_days/Day12_hingeAnimation.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
 //In Dart, the late keyword is used to declare a non-nullable variable that will be initialized after its declaration but before it is used for the first time.
   late AnimationController _controller;
-  late Animation rotationAnimation;
+  late Animation<double> rotationAnimation;
   late Animation<double> slideAnimation;
   late Animation<double> opacityAnimation;
 
@@ -40,9 +40,12 @@ class _HomePageState extends State<HomePage>
     _controller = AnimationController(
         vsync: this, duration: const Duration(seconds: 200));
 
-    rotationAnimation = Tween(end: 0.15, begin: 0.0).animate(CurvedAnimation(
+    rotationAnimation = Tween<double>(begin: 0.0, end: 0.15).animate(
+      CurvedAnimation(
         parent: _controller,
-        curve: const Interval(curve: Curves.bounceOut, 0.0, 0.5)));
+        curve: const Interval(0.0, 0.5, curve: Curves.bounceOut),
+      ),
+    );
 
     slideAnimation = Tween(begin: 100.0, end: 600.0).animate(CurvedAnimation(
         parent: _controller,
@@ -54,6 +57,9 @@ class _HomePageState extends State<HomePage>
   }
 
 //Called when this object is removed from the tree permanently.
+//It is overridden to perform necessary cleanup activities when a widget or its state is permanently removed from the widget tree.
+//in this case it is used to release the resources used by this object to prevent memory leaks.
+//A memory leak in programming occurs when a computer program incorrectly manages memory allocations, resulting in reduced performance or system crashes.
   @override
   void dispose() {
     super.dispose();
@@ -64,8 +70,37 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          //title: Text(title),
-          ),
+        title: const Text("Hinge Animation"),
+      ),
+      body: AnimatedBuilder(
+        animation: slideAnimation,
+        builder: (context, child) {
+          return Container(
+            width: 200,
+            height: 150,
+            padding: const EdgeInsets.all(0),
+            margin: EdgeInsets.only(left: 100, top: slideAnimation.value),
+            child: RotationTransition(
+              turns: rotationAnimation,
+              child: const Center(
+                child: Text(
+                  "Hinge Animation",
+                  style: TextStyle(color: Colors.pink),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.play_arrow),
+        backgroundColor: Colors.green[300],
+        onPressed: () {
+          _controller.forward();
+        },
+      ),
     );
   }
 }
