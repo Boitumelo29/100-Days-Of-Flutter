8d0f5faedc88764a7a3d8859238312ed4c32493a
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-26 17:53:36 +0200

    update

--- lib/flutter_100_days/Day14_formValidation.dart
+++ lib/flutter_100_days/Day14_formValidation.dart
@@ -24,11 +24,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _formKey = GlobalKey<FormState>();
+  var isLoading = false;
+
+  void _submit() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      // _formKey.currentContext!.save();
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: const Column(
+            children: <Widget>[Text("Form-Validation In Flutter")],
+          ),
+        ),
       ),
     );
   }
