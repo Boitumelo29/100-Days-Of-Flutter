84bc204ef4f6de7fda361f84a43a269ede01299a
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-26 17:44:05 +0200

    update

--- lib/flutter_100_days/Day14_formValidation.dart
+++ lib/flutter_100_days/Day14_formValidation.dart
@@ -5,8 +5,31 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final String title = "Form Validation";
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(title: title),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
   }
 }
