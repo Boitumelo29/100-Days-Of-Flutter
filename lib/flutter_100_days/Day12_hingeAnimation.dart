5287d7bd77c01f378d943943da6da01812bb7ea5
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-26 16:32:32 +0200

    update

--- lib/flutter_100_days/Day12_hingeAnimation.dart
+++ lib/flutter_100_days/Day12_hingeAnimation.dart
@@ -10,6 +10,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(title: title),
+    );
+  }
+}
+
+//hinge animation is an animation where an element can turn over with a sudden movement
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _rotationAnimation;
+  Animation<double> _slideAnimation;
+  Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          //title: Text(title),
+          ),
     );
   }
 }
