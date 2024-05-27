bc98aa045b90ace849c7d005aef9dbbcfbe43a72
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-27 19:16:51 +0200

    update

--- lib/flutter_100_days/Day18_routes.dart
+++ lib/flutter_100_days/Day18_routes.dart
@@ -19,6 +19,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SecondPage())),
+          child: const Text("Navigate To Next Page"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Page"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Go Back"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
