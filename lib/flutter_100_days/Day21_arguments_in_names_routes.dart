86268fc425ff7b492b89af64f7c921e00e6f0f14
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-28 10:53:54 +0200

    update

---
+++ lib/flutter_100_days/Day21_arguments_in_names_routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  final String title = "Arguments in Names Routes";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage(title: title));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
