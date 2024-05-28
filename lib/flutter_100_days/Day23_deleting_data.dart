76508e30776a3d45f73376b9a843689877c43669
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-28 19:59:58 +0200

    update

---
+++ lib/flutter_100_days/Day23_deleting_data.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  final String title = "Deleting From Internet";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(title: title),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(future: ,builder: (context, snapshot) {
+        
+      },),
+    );
+  }
+}
+
+
+class Album {
+  final int id;
+  final String title;
+
+  Album({required this.id, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json){
+    return Album(id: json['id'], title: json['title']);
+  }
+}
