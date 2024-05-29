1f6255d99df4fb2fdc57b48a1680a234a705bb83
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-29 12:16:41 +0200

    update

--- lib/flutter_100_days/Day23_deleting_data.dart
+++ lib/flutter_100_days/Day23_deleting_data.dart
@@ -27,65 +27,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<Album> _futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder(future: ,builder: (context, snapshot) {
-        
-      },),
+      body: FutureBuilder(
+        future: _futureAlbum,
+        builder: (context, snapshot) {},
+      ),
     );
   }
 
-  Future<Album> fetchAlbum()async{
-
+  Future<Album> fetchAlbum() async {
     var uri = 'https://jsonplaceholder.typicode.com/albums/1';
     final response = await http.get(Uri.parse(uri));
 
     if (response.statusCode == 200) {
-      return Album.fromJson(jsonDecode(response.body,),);
-    }else{
+      return Album.fromJson(
+        jsonDecode(
+          response.body,
+        ),
+      );
+    } else {
       var snackBar = const SnackBar(content: Text("An error has occured"));
       throw ScaffoldMessenger(child: snackBar);
     }
-
   }
 }
 
- Future<Album> updateAlbum(String title) async {
-    const String uri = 'https://jsonplaceholder.typicode.com/albums/1';
-    final response = await http.put(Uri.parse(uri),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8'
-        },
-        body: jsonEncode(<String, String>{'title': title}));
+Future<Album> updateAlbum(String title) async {
+  const String uri = 'https://jsonplaceholder.typicode.com/albums/1';
+  final response = await http.put(Uri.parse(uri),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{'title': title}));
 
-    if (response.statusCode == 200) {
-      return Album.fromJson(jsonDecode(response.body));
-    } else {
-      var snackBar = const SnackBar(content: Text("An error has occures"));
-      throw ScaffoldMessenger(child: snackBar);
-    }
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    var snackBar = const SnackBar(content: Text("An error has occures"));
+    throw ScaffoldMessenger(child: snackBar);
   }
+}
 
-Future<Album> deleteAlbum(String id) async { 
-   var uri = 'https://jsonplaceholder.typicode.com/albums/$id';
+Future<Album> deleteAlbum(String id) async {
+  var uri = 'https://jsonplaceholder.typicode.com/albums/$id';
 
-  final http.Response response = await http.delete( Uri.parse(uri), 
-    headers: <String, String>{ 
-      'Content-Type': 'application/json; charset=UTF-8', 
-    }, 
-  ); 
-  
-  if (response.statusCode == 200) { 
-    return Album.fromJson(jsonDecode(response.body)); 
-  } else { 
-    throw Exception('Item Not Deleted!'); 
-  } 
-} 
+  final http.Response response = await http.delete(
+    Uri.parse(uri),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
 
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Item Not Deleted!');
+  }
+}
 
 class Album {
   final int id;
@@ -93,7 +104,7 @@ class Album {
 
   Album({required this.id, required this.title});
 
-  factory Album.fromJson(Map<String, dynamic> json){
+  factory Album.fromJson(Map<String, dynamic> json) {
     return Album(id: json['id'], title: json['title']);
   }
 }
