f03186655fdd7f9255dbce5457015437063561a1
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-29 17:44:28 +0200

    update

--- lib/practice_app/fetching_data.dart
+++ lib/practice_app/fetching_data.dart
@@ -4,11 +4,37 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  final String title = "Fetching Data From The Internet";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(title: title),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
     );
   }
 }
