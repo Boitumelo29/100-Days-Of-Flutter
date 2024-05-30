729613e9e318bd68720827690aa83bcf6387c72f
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-30 20:17:33 +0200

    update

--- lib/practice_app/pract_store.dart
+++ lib/practice_app/pract_store.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:skeleton_text/skeleton_text.dart';
 
 void main() => runApp(const MyApp());
 
@@ -40,28 +41,95 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     //fetchData();
     return Scaffold(
+      drawer: Drawer(
+          child: ListView(
+        children: const <Widget>[
+          UserAccountsDrawerHeader(
+              currentAccountPicture: Icon(
+                Icons.person,
+                size: 40,
+              ),
+              decoration: BoxDecoration(color: Colors.grey),
+              accountName: Text("Account: Boitumelo"),
+              accountEmail: Text("Email: tumijane@gmail.com")),
+          ListTile(
+            title: Text("Cart"),
+            trailing: Icon(Icons.shopping_cart),
+          ),
+          ListTile(
+            title: Text("Log out"),
+            trailing: Icon(Icons.exit_to_app),
+          )
+        ],
+      )),
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: ListView.builder(
           itemCount: products.length,
           itemBuilder: (context, index) {
-            return SizedBox(
-              height: 80,
-              width: 80,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: Image.network(products[index].image),
+            if (products.isEmpty) {
+              return SizedBox(
+                height: 80,
+                width: 80,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: Image.network(products[index].image),
+                    ),
+                    title: Text(products[index].title),
+                    subtitle: Text("R${products[index].price}"),
                   ),
-                  title: Text(products[index].title),
-                  subtitle: Text("R${products[index].price}"),
                 ),
-              ),
-            );
+              );
+            }
+            return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    SkeletonAnimation(
+                      child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width * 0.2,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(5))),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SkeletonAnimation(
+                          child: Container(
+                            height: 15,
+                            width: MediaQuery.of(context).size.width * 0.6,
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SkeletonAnimation(
+                          child: Container(
+                            height: 15,
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ));
           }),
     );
   }
