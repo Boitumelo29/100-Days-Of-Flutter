9967974412cd3fa0eccf6506fcc6d5d053f23202
Boitumelo Thobejane <tumijane99@gmail.com>
2024-05-31 19:11:43 +0200

    update

--- lib/practice_app/rick_and_morty_api.dart
+++ lib/practice_app/rick_and_morty_api.dart
@@ -5,10 +5,38 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final String title = "Rick And Mortey API Call";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(title: title)
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+//The model
+
+class Character{
+  final String id;
+  final String name;
+  final String species
+  final String 
 }
