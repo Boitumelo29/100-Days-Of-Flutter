0692c1b441d0003c969b160285863adc9b407323
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-31 08:38:09 +0200

    update

---
+++ lib/practice_app/my_joke_app_api.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  final String title = "My Joke App";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage(title: title));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
