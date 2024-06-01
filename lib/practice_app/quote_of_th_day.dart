33feed144d40feceb58df9f18a690dc3dd633653
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-01 18:19:43 +0200

    update

---
+++ lib/practice_app/quote_of_th_day.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  final String title = "Quote Of The Day API APP";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage(title: title));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
