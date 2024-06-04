383c7d62b5ff7b5c695735c1c3b07f0720eaad79
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-04 09:08:35 +0200

    update

--- lib/flutter_100_days/Day24_image_picker.dart
+++ lib/flutter_100_days/Day24_image_picker.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final String title = "Image Picker";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage(title: title));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
