267fe7dd25b31dd935576e8c377a0bc59864e9e2
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-04 09:06:08 +0200

    update

--- lib/practice_app/quote_of_th_day.dart
+++ lib/practice_app/quote_of_th_day.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(const MyApp());
 
@@ -14,23 +17,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
 
   const HomePage({super.key, required this.title});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<QuoteOfThDay> qoute;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    qoute = fetchData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder<QuoteOfThDay>(
+        future: qoute,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text(snapshot.data!.author),
+                  const SizedBox(),
+                  Text(snapshot.data!.body)
+                ],
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
 }
 
-//the model
+//fetching the data
 
+Future<QuoteOfThDay> fetchData() async {
+  var uri = "https://favqs.com/api/qotd";
+  final response = await http.get(Uri.parse(uri));
+  print(response.body);
+  if (response.statusCode == 200) {
+    return QuoteOfThDay.fromJson(json.decode(response.body));
+  }
+  throw "";
+}
+
+//the model
 class QuoteOfThDay {
   final String author;
   final String body;
@@ -38,6 +85,8 @@ class QuoteOfThDay {
   QuoteOfThDay({required this.author, required this.body});
 
   factory QuoteOfThDay.fromJson(Map<String, dynamic> json) {
-    return QuoteOfThDay(author: json['author'], body: json['body']);
+    var quoteJson = json['quote'];
+    return QuoteOfThDay(
+        author: quoteJson['author'], body: quoteJson['body'] ?? "null");
   }
 }
