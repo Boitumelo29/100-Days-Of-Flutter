d3e1b99a70a31f7b7d0041241b4b5f7f7664fbce
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-05 16:44:29 +0200

    update

--- lib/flutter_100_days/Day25_restrict_landscape.dart
+++ lib/flutter_100_days/Day25_restrict_landscape.dart
@@ -21,6 +21,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text("This App Can Not Be Rotated To LandScape Mode"),
+      ),
+    );
   }
 }
