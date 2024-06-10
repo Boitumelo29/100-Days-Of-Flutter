9d950fed632b3ca25a043137956d26ee12f5fd7c
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-10 09:42:42 +0200

    update

--- lib/flutter_100_days/Day26_fetching_data_from_api_through_dio.dart
+++ lib/flutter_100_days/Day26_fetching_data_from_api_through_dio.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'dart:convert';
@@ -28,9 +29,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var jsonList;
+
   @override
   void initState() {
     super.initState();
+    getData();
   }
 
   @override
@@ -38,13 +42,42 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
       ),
+      body: ListView.builder(
+          itemCount: jsonList == null ? 0 : jsonList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(80),
+                  child: Image.network(
+                    jsonList[index]['url'],
+                    fit: BoxFit.fill,
+                    width: 50,
+                    height: 50,
+                  ),
+                ),
+                title: Text(jsonList[index]['name']),
+                subtitle: Text(jsonList[index]['power']),
+              ),
+            );
+          }),
     );
   }
 
   void getData() async {
     try {
-      var response = await Dio().get('https://protocoderspoint.com/jsondata/superheros.json');
+      var response = await Dio()
+          .get('https://protocoderspoint.com/jsondata/superheros.json');
+
+      if (response.statusCode == 200) {
+        setState(() {
+          jsonList = response.data['superheros'] as List;
+        });
+      } else {
+        print(response.statusCode);
+      }
     } catch (e) {
       print(e);
     }
