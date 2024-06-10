555e2a7e3be01ced4c2e22f3b04f254fdf5397ad
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-10 09:27:00 +0200

    update

---
+++ lib/flutter_100_days/Day26_fetching_data_from_api_through_dio.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  final String title = " Fetching List of Data From API Through Dio";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
