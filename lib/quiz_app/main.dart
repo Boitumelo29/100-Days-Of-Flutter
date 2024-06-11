cc8b2b5743c58ac5338a51a543b641629f25d494
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-11 16:49:55 +0200

    update

---
+++ lib/quiz_app/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  final String title = "My Quiz";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(title: title),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
